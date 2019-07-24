725364e8911d23440b3ea04af226f675a7cd1481
Jota <mfjotaa@hotmail.com>
2019-07-24 17:08:17 +0200

    Stop-words

--- grandpyapp/views.py
+++ grandpyapp/views.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, url_for, request, jsonify
 from mediawiki import MediaWiki
 from stop_words import get_stop_words, safe_get_stop_words
+import json
 
 app = Flask(__name__)
 # Config options - Make sure you created a 'config.py' file.
@@ -15,23 +16,25 @@ def index():
 def get_prediction():
     wikipedia = MediaWiki()
     word = request.args.get('word')
-
-    """stop_words = get_stop_words('fr')
+    stop_words = get_stop_words('fr')
     stop_words = get_stop_words('french')
     stop_words = safe_get_stop_words('unsupported language')
     # split query
     filtered_sentence = ""
     filtered_sentence = word.split()
+    #data = request.get_json('fr.json') 
+    #print(data)
+
+    reponse =  []
     print(filtered_sentence)
     for each in filtered_sentence:
+        print(each)
         if each not in stop_words:
-            print(each)
-            filtered_sentence.append(each)"""
-
-    print(wikipedia.search(word))
-    print(wikipedia.geosearch(title=word))
+            reponse.append(each)
+            print(reponse)
+    #print(wikipedia.search(word))
+    #print(wikipedia.geosearch(title=word))
     return jsonify({'html': word})
-
-
+     
 if __name__ == "__main__":
     app.run()
