f3e73fb7c731387cee37754b8170be90c96a85e3
Joana Silva <mfjotaa@hotmail.com>
2019-07-29 19:10:31 +0200

    Google_api

--- grandpyapp/static/js/grandpy.js
+++ grandpyapp/static/js/grandpy.js
@@ -7,59 +7,36 @@ window.addEventListener("load", function () {
     // Ajout d'un gestionnaire pour l'événement click
     boutonElt.addEventListener("click", function () {
       console.log("clic");
-      var form = document.querySelector("form");
-      console.log(form);
-      // Gestion de la soumission du formulaire
-      form.addEventListener("submit", function (e) {
-          e.preventDefault();
-          var query = form.elements.query.value;
-          
-          if (query == "") {
-            alert("Question must be filled out");
-            return false;
-          }
-          console.log("Vous avez choisi la question " + query );
-        
-          e.preventDefault(); // Annulation de l'envoi des données
-          // Envoi des données du formulaire au serveur
-          // La fonction callback est ici vide
-          /*"https://www.mediawiki.org/w/api.php?action=query&format=json"*/
-          ajaxPost("https://www.mediawiki.org/w/api.php", query, function (reponse) {
-            // Le film est affiché dans la console en cas de succès
-            console.log("Le film " + JSON.stringify(query) + " a été envoyé au serveur");
-            },
-            true // Valeur du paramètre isJson
-        );
-      });
 
+      var form = document.querySelector("form");
       // Affiche de toutes les données saisies ou choisies
-      /*form.addEventListener("submit", function (e) {
+      form.addEventListener("submit", function (e) {
           var query = form.elements.query.value;
-          
           if (query == "") {
             alert("Question must be filled out");
             return false;
-          }
+        }
         console.log("Vous avez choisi la question " + query );
         
         e.preventDefault(); // Annulation de l'envoi des données
 
-        ajaxGet("https://www.mediawiki.org/w/api.php?action=query&list=search&srsearch="+query+"&utf8=&format=json", function (reponse) {
-          console.log(reponse);
-          var films = JSON.parse(reponse);
-          // Affiche le titre de chaque film
-          films.forEach(function (film) {
-            console.log(film.titre);
-          })
-        });
-      });*/
+        function myMap() {
+          var mapProp = {
+            zoom:8,
+            center:new google.maps.LatLng(51.508742,-0.120850)
+          }
+          var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
 
-      /*function myMap() {
-      var mapProp= {
-        center:new google.maps.LatLng(51.508742,-0.120850),
-        zoom:5,
-      };
-      var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
-      }*/
+          /*var marker = new google.maps.marker({
+            position:new google.maps.LatLng(51.508742,-0.120850),
+            map = map
+          });*/
+          }
+        /*ajaxGet("https://www.mediawiki.org/w/api.php?action=query&list=search&srsearch="+query+"&utf8=&format=json", function (reponse) {
+        console.log(reponse);
+        });*/
+      });
+
+      
     });  
 });
--- grandpyapp/views.py
+++ grandpyapp/views.py
@@ -1,6 +1,10 @@
 from flask import Flask, render_template, url_for, request, jsonify
 from mediawiki import MediaWiki
 from stop_words import get_stop_words, safe_get_stop_words
+import requests
+import urllib.request, urllib.parse, urllib.error
+import googlemaps
+import json
 
 app = Flask(__name__)
 # Config options - Make sure you created a 'config.py' file.
@@ -15,23 +19,76 @@ def index():
 def get_prediction():
     wikipedia = MediaWiki()
     word = request.args.get('word')
-
-    """stop_words = get_stop_words('fr')
+    stop_words = get_stop_words('fr')
     stop_words = get_stop_words('french')
-    stop_words = safe_get_stop_words('unsupported language')
+    print(stop_words)
+
     # split query
     filtered_sentence = ""
     filtered_sentence = word.split()
+
+    reponse = []
     print(filtered_sentence)
     for each in filtered_sentence:
+        print(each)
         if each not in stop_words:
-            print(each)
-            filtered_sentence.append(each)"""
+            reponse.append(each)
+            print(reponse)
 
-    print(wikipedia.search(word))
-    print(wikipedia.geosearch(title=word))
-    return jsonify({'html': word})
+    string_query = ' '.join(reponse)
+
+    # serviceurl = 'https://www.google.com/maps/embed/v1/place?'
+    """serviceurl = 'https://maps.googleapis.com/maps/api/geocode/json?'
+
+    while True:
+        address = string_query
+
+        "if len(address) < 1:
+            break
+
+        url = serviceurl + "key=" + app.config['KEY_API'] + "&" + urllib.parse.urlencode({'address': address})
+        print('retrieving', url)
+
+        uh = urllib.request.urlopen(url)
+        data = uh.read().decode()
+
+        try:
+            js = json.loads(data)
+            print(js)
+        except:
+            js = None
+            # search_json = None
+
+        if not js:
+            if 'status' not in js:
+                if js['status'] != 'OK':
+                    print('==== Failure To Retrieve ====')
+                    print(js)
+                    continue
+
+        lat = js["results"][0]["geometry"]["location"]["lat"]
+        lng = js["results"][0]["geometry"]["location"]["lng"]
+
+        print('lat', lat, 'lng', lng)"""
+
+    """location = js['results'][0]['formatted_address']
+
+        print(location)"""
+
+    # print(string_query)
+    
+    lat = '48.856614'
+    lng = '2.3522219'
+    query = wikipedia.geosearch(lat, lng)
+    print(wikipedia.geosearch(lat, lng))
+
+    history = query[0]
+
+    print(history)
+    print(wikipedia.summary(history))
+    summary = wikipedia.summary(history)
+    return jsonify({'html': summary})
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
