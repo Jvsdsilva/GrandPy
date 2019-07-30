7f243a26cdd0e1085a40c58774c21e033c11807f
Joana Silva <mfjotaa@hotmail.com>
2019-07-30 18:53:21 +0200

    map

--- grandpyapp/static/js/grandpy.js
+++ grandpyapp/static/js/grandpy.js
@@ -1,15 +1,15 @@
-// Gestion de la fin du chargement de la page web
+// Management of the end of the loading of the web page
 window.addEventListener("load", function () {
     console.log("Page entièrement chargée");
   
     var boutonElt = document.getElementById("search");
     
-    // Ajout d'un gestionnaire pour l'événement click
+    // Adding a manager for the click event
     boutonElt.addEventListener("click", function () {
       console.log("clic");
 
       var form = document.querySelector("form");
-      // Affiche de toutes les données saisies ou choisies
+      // Displays all data entered or selected
       form.addEventListener("submit", function (e) {
           var query = form.elements.query.value;
           if (query == "") {
@@ -18,9 +18,9 @@ window.addEventListener("load", function () {
         }
         console.log("Vous avez choisi la question " + query );
         
-        e.preventDefault(); // Annulation de l'envoi des données
+        e.preventDefault(); // Cancel sending data
 
-        function myMap() {
+        /*function myMap() {
           var mapProp = {
             zoom:8,
             center:new google.maps.LatLng(51.508742,-0.120850)
@@ -30,13 +30,43 @@ window.addEventListener("load", function () {
           /*var marker = new google.maps.marker({
             position:new google.maps.LatLng(51.508742,-0.120850),
             map = map
-          });*/
-          }
+          });
+          }*/
         /*ajaxGet("https://www.mediawiki.org/w/api.php?action=query&list=search&srsearch="+query+"&utf8=&format=json", function (reponse) {
         console.log(reponse);
         });*/
-      });
 
-      
+        $(document).ready(function() {
+          $('#search').click(function(){
+              var word = $('#query').val();
+              $.ajax({
+              url: "/get_coord",
+              type: "get",
+              data: {word: word},
+              success: function(response) {
+                for (var i = 0; i < response.features.length; i++) {
+                  var coords = response.features[i].geometry.coordinates;
+                  var latLng = new google.maps.LatLng(coords[1],coords[0]);
+                  var marker = new google.maps.Marker({
+                    position: latLng,
+                    map: map
+                  });
+                }
+                var mapProp = {
+                  zoom:8,
+                  center:latLng
+                }
+                var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+
+               /*position:new google.maps.LatLng($("#googleMap").html(response.html));*/
+              
+             },
+             error: function(xhr) {
+              console.log("erreur summary")
+            }
+            });
+          });
+       });
+      }); 
     });  
 });
--- grandpyapp/views.py
+++ grandpyapp/views.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, url_for, request, jsonify
+from flask_googlemaps import GoogleMaps
+from flask_googlemaps import Map
 from mediawiki import MediaWiki
 from stop_words import get_stop_words, safe_get_stop_words
 import requests
@@ -10,6 +12,8 @@ app = Flask(__name__)
 # Config options - Make sure you created a 'config.py' file.
 app.config.from_object('config')
 # To get one variable, tape app.config['MY_VARIABLE']
+# Initialize the extension
+GoogleMaps(app)
 
 @app.route('/')
 def index():
@@ -27,12 +31,7 @@ def get_prediction():
     filtered_sentence = ""
     filtered_sentence = word.split()
 
-
-    #data = request.get_json('fr.json') 
-    #print(data)
-
-    reponse =  []
-
+    reponse = []
     print(filtered_sentence)
     for each in filtered_sentence:
         print(each)
@@ -48,7 +47,7 @@ def get_prediction():
     while True:
         address = string_query
 
-        "if len(address) < 1:
+        if len(address) < 1:
             break
 
         url = serviceurl + "key=" + app.config['KEY_API'] + "&" + urllib.parse.urlencode({'address': address})
@@ -92,8 +91,105 @@ def get_prediction():
     print(history)
     print(wikipedia.summary(history))
     summary = wikipedia.summary(history)
-    return jsonify({'html': summary})
 
+    return jsonify({'html': summary})
+    
+@app.route('/get_coord')
+def get_coordinates():
+    word = request.args.get('word')
+    stop_words = get_stop_words('fr')
+    stop_words = get_stop_words('french')
+    latlng = []
+    print(stop_words)
+
+    # split query
+    filtered_sentence = ""
+    filtered_sentence = word.split()
+
+    reponse = []
+    print(filtered_sentence)
+    for each in filtered_sentence:
+        print(each)
+        if each not in stop_words:
+            reponse.append(each)
+            print(reponse)
+
+    string_query = ' '.join(reponse)
+
+    # serviceurl = 'https://www.google.com/maps/embed/v1/place?'
+    """serviceurl = 'https://maps.googleapis.com/maps/api/geocode/json?'
+
+    while True:
+        address = string_query
+
+        if len(address) < 1:
+            break
+
+        url = serviceurl + "key=" + app.config['KEY_API'] + "&" + urllib.parse.urlencode({'address': address})
+        print('retrieving', url)
+
+        uh = urllib.request.urlopen(url)
+        data = uh.read().decode()
+
+        try:
+            js = json.loads(data)
+            print(js)
+        except:
+            js = None
+            # search_json = None
+
+        if not js:
+            if 'status' not in js:
+                if js['status'] != 'OK':
+                    print('==== Failure To Retrieve ====')
+                    print(js)
+                    continue
+
+        lat = js["results"][0]["geometry"]["location"]["lat"]
+        lng = js["results"][0]["geometry"]["location"]["lng"]
+
+        print('lat', lat, 'lng', lng)"""
+
+    """location = js['results'][0]['formatted_address']
+
+        print(location)"""
+
+    # print(string_query)
+    
+    lat = '48.856614'
+    lng = '2.3522219'
+    latlng.append(lat)
+    latlng.append(lng)
+
+    # creating a map in the view
+    mymap = Map(
+        identifier="view-side",
+        lat=lat,
+        lng=lng,
+        markers=[(latlng[0], latlng[1])]
+    )
+    sndmap = Map(
+        identifier="sndmap",
+        lat=lat,
+        lng=lng,
+        markers=[
+    {
+    'icon': 'http://maps.google.com/mapfiles/ms/icons/green-dot.png',
+    'lat': lat,
+    'lng': lng,
+    'infobox': "<b>Hello World</b>"
+    },
+    {
+    'icon': 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png',
+    'lat': lat,
+    'lng': lng,
+    'infobox': "<b>Hello World from other place</b>"
+    }
+    ]
+    )
+    return render_template('base.html', mymap=mymap, sndmap=sndmap)
+
+    #return jsonify({'html': latlng})
 
 if __name__ == "__main__":
     app.run(debug=True)
