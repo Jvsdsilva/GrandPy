5b060a022a1ecb639cb85ed95a0ae6fb7af18a33
Jvsdsilva <mfjotaa@hotmail.com>
2019-08-03 11:40:42 +0200

    Corrections

--- config.py
+++
@@ -1,6 +0,0 @@
-# To generate a new secret key:
-# >>> import random, string
-# >>> "".join([random.choice(string.printable) for _ in range(24)])
-SECRET_KEY = "D=x25 _Slo,%In\t$\\wV\n\\9^V"
-KEY_API = "AIzaSyC_GqYtR8bi55nXC3FdvJ5cDWbN-QCXNM0"
-
