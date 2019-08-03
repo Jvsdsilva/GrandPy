81fbb39c5813a70d300887c799e31371325fbec2
Jvsdsilva <mfjotaa@hotmail.com>
2019-08-03 11:31:39 +0200

    Corrections

--- config.py
+++ config.py
@@ -2,4 +2,5 @@
 # >>> import random, string
 # >>> "".join([random.choice(string.printable) for _ in range(24)])
 SECRET_KEY = "D=x25 _Slo,%In\t$\\wV\n\\9^V"
-KEY_API = "AIzaSyDHZPTAlm4YpD_EYfHeUmNv3gNSkEI-By4"
+KEY_API = "AIzaSyC_GqYtR8bi55nXC3FdvJ5cDWbN-QCXNM0"
+
--- grandpyapp/views.py
+++ grandpyapp/views.py
@@ -23,9 +23,9 @@ def index():
 def get_prediction():
     wikipedia = MediaWiki()
     word = request.args.get('word')
-    stop_words = get_stop_words('fr')
-    stop_words = get_stop_words('french')
-    #print(stop_words)
+    # Set stop words language
+    stop_words = get_stop_words('en')
+    stop_words = get_stop_words('english')
 
     # split query
     filtered_sentence = ""
@@ -33,16 +33,13 @@ def get_prediction():
 
     reponse = []
 
-    print(filtered_sentence)
     for each in filtered_sentence:
-        print(each)
         if each not in stop_words:
             reponse.append(each)
             print(reponse)
 
     string_query = ' '.join(reponse)
 
-    # serviceurl = 'https://www.google.com/maps/embed/v1/place?'
     """serviceurl = 'https://maps.googleapis.com/maps/api/geocode/json?'
 
     while True:
@@ -50,75 +47,69 @@ def get_prediction():
 
         if len(address) < 1:
             break
-
-        url = serviceurl + "key=" + app.config['KEY_API'] + "&" + urllib.parse.urlencode({'address': address})
-        print('retrieving', url)
-
-        uh = urllib.request.urlopen(url)
-        data = uh.read().decode()
-
+        
         try:
+            url = serviceurl + "key=" + app.config['KEY_API'] + "&" + urllib.parse.urlencode({'address': address})
+            print('retrieving', url)
+            
+            uh = urllib.request.urlopen(url)
+            data = uh.read().decode()
             js = json.loads(data)
-            print(js)
-        except:
+            #print(js)
+        except e:
+            print('==== Failure To Retrieve ===='+ e)
             js = None
-            # search_json = None
-
+            #print(js)
+        
         if not js:
             if 'status' not in js:
                 if js['status'] != 'OK':
                     print('==== Failure To Retrieve ====')
                     print(js)
                     continue
+        else:
+            lat = js["results"][0]["geometry"]["location"]["lat"]
+            lng = js["results"][0]["geometry"]["location"]["lng"]
 
-        lat = js["results"][0]["geometry"]["location"]["lat"]
-        lng = js["results"][0]["geometry"]["location"]["lng"]
+            print('lat', lat, 'lng', lng)"""
 
-        print('lat', lat, 'lng', lng)"""
-
-    """location = js['results'][0]['formatted_address']
-
-        print(location)"""
-
-    # print(string_query)
-    
+            # print(string_query)
+            
     lat = '48.856614'
     lng = '2.3522219'
 
+    # sent coordinates to Media wiki
     query = wikipedia.geosearch(lat, lng)
-    #print(wikipedia.geosearch(lat, lng))
 
+    # Save first answer
     history = query[0]
 
-    #print(history)
-    #print(wikipedia.summary(history))
+    # sent answer to Media wiki
     summary = wikipedia.summary(history)
 
+    # return summary to view html
     return jsonify({'html': summary})
     
 @app.route('/get_coord')
 def get_coordinates():
-    word = request.args.get('word')
-    stop_words = get_stop_words('fr')
-    stop_words = get_stop_words('french')
     latlng = []
-    #print(stop_words)
+    reponse = []
+    word = request.args.get('word')
+    # Set stop words language
+    stop_words = get_stop_words('en')
+    stop_words = get_stop_words('english')
 
     # split query
     filtered_sentence = ""
     filtered_sentence = word.split()
 
-    reponse = []
-    #print(filtered_sentence)
     for each in filtered_sentence:
-        #print(each)
         if each not in stop_words:
             reponse.append(each)
             #print(reponse)
 
     string_query = ' '.join(reponse)
 
-    # serviceurl = 'https://www.google.com/maps/embed/v1/place?'
     """serviceurl = 'https://maps.googleapis.com/maps/api/geocode/json?'
 
     while True:
@@ -126,13 +117,17 @@ def get_coordinates():
 
         if len(address) < 1:
             break
-
-        url = serviceurl + "key=" + app.config['KEY_API'] + "&" + urllib.parse.urlencode({'address': address})
-        print('retrieving', url)
-
-        uh = urllib.request.urlopen(url)
-        data = uh.read().decode()
-
+        try:
+            url = serviceurl + "key=" + app.config['KEY_API'] + "&" + urllib.parse.urlencode({'address': address})
+        except:
+            print('retrieving', url)
+        
+        try:
+            uh = urllib.request.urlopen(url)
+            data = uh.read().decode()
+            
+        except:
+            print('==== Failure To Retrieve ===='+ uh)
         try:
             js = json.loads(data)
             print(js)
@@ -146,17 +141,11 @@ def get_coordinates():
                     print('==== Failure To Retrieve ====')
                     print(js)
                     continue
+        else:
+            lat = js["results"][0]["geometry"]["location"]["lat"]
+            lng = js["results"][0]["geometry"]["location"]["lng"]
 
-        lat = js["results"][0]["geometry"]["location"]["lat"]
-        lng = js["results"][0]["geometry"]["location"]["lng"]
-
-        print('lat', lat, 'lng', lng)
-
-    location = js['results'][0]['formatted_address']
-
-    print(location)"""
-
-    # print(string_query)
+            print('lat', lat, 'lng', lng)"""
     
     lat = '48.856614'
     lng = '2.3522219'
@@ -165,7 +154,9 @@ def get_coordinates():
     
     location = json.dumps(latlng)
 
+    # Return new coordinates to reload map view html
     return jsonify({'html': location})
 
+        
 if __name__ == "__main__":
     app.run(debug=True)
